80c5d7d0ad503255b127026e9f536eed52ed07c4
B.M.Samiul Haque Real <samiulreal198@gmail.com>
2023-11-29 23:58:20 +0600

    1st

---
+++ android/app/src/main/kotlin/com/example/flowwidget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flowwidget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  IconData lastIconClicked = Icons.notifications;
+
+  final List<IconData> menuItem = <IconData>[
+    Icons.menu,
+    Icons.home,
+    Icons.add_alert,
+    Icons.search,
+    Icons.satellite_alt,
+  ];
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = AnimationController(vsync: this, duration: Duration(milliseconds: 250));
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    controller.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Flow(
+          delegate: FlowMenuDelegate(menuAnimation: controller),
+          children: menuItem
+              .map<Widget>((IconData icon) => Padding(
+                    padding: EdgeInsets.all(5),
+                    child: FloatingActionButton(
+                      foregroundColor: Colors.white,
+                      elevation: 0,
+                      backgroundColor:
+                          lastIconClicked == icon ? Colors.green : Colors.grey,
+                      splashColor: Colors.green,
+                      onPressed: () {
+                        if (icon != Icons.satellite_alt) {
+                          setState(() {
+                            lastIconClicked = icon;
+                          });
+                          controller.status == AnimationStatus.completed
+                              ? controller.reverse()
+                              : controller.forward();
+                        }
+                      },
+                      child: Icon(icon),
+                    ),
+                  ))
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class FlowMenuDelegate extends FlowDelegate {
+  FlowMenuDelegate({required this.menuAnimation})
+      : super(repaint: menuAnimation);
+
+  final Animation<double> menuAnimation;
+
+  @override
+  bool shouldRepaint(FlowMenuDelegate oldDelegate) {
+    return menuAnimation != oldDelegate.menuAnimation;
+  }
+
+  @override
+  void paintChildren(FlowPaintingContext context) {
+    //---------------Top Show----------------
+    double dx = 0.0;
+    /*for (int i = 0; i < context.childCount; i++) {
+      dx = context.getChildSize(i)!.width * i;
+      context.paintChild(i, transform: Matrix4.translationValues(dx * menuAnimation.value, 0, 0)); // Main Line of icon position
+    }*/
+
+    //--------------Bottom show --------------
+
+    final size = context.size;
+    final xStart = size.width - 80;
+    final yStart = size.height - 80;
+    for (int i = context.childCount - 1; i >=0; i--) {
+      dx = context.getChildSize(i)!.width * i;
+      //final x = xStart - dx * 0.9 * menuAnimation.value; // Digonal
+      //final y = yStart - dx * 0.9 * menuAnimation.value;  // Digonal
+      final x = xStart;  // Horizontal ( - dx )
+      final y = yStart - dx * menuAnimation.value;  // Vertical ( - dx )
+      context.paintChild(i, transform: Matrix4.translationValues(x, y, 0)); // Main Line of icon position
+    }
+
+  }
+}
